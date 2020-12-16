878e58d46312fc7a5430960cf9f6f9b5fc498746
dasd412 <45190017+dasd412@users.noreply.github.com>
2020-12-16 15:27:16 +0900

    jpa practice 01

--- src/main/java/org/dasd/persistence/BoardRepository.java
+++ src/main/java/org/dasd/persistence/BoardRepository.java
@@ -2,10 +2,40 @@ package org.dasd.persistence;
 
 
 import org.dasd.domain.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
+
+
+import java.util.Collection;
+import java.util.List;
 
 public interface BoardRepository extends CrudRepository<Board,Long> {
 
      Board findByBno(long id);
+
+     List<Board>findBoardByTitle(String title);
+
+     Collection<Board>findByWriter(String writer);
+
+     //like %
+     Collection<Board>findByWriterContaining(String writer);
+
+     //Or를 이용해 title과 content에 특정한 문자열이 있는지 검색
+     Collection<Board>findByTitleContainingOrContentContaining(String title,String content);
+
+     //title like %?% and bno>?
+     Collection<Board>findByTitleContainingAndBnoGreaterThan(String title, Long num);
+
+
+     //bno>? order by bno desc
+     Collection<Board>findByBnoGreaterThanOrderByBnoDesc(Long bno);
+
+     //bno>? order by bno desc limit ?,?
+     List<Board>findByBnoGreaterThanOrderByBnoDesc(Long bno, Pageable paging);
+
+
+     //noSorting
+     Page<Board> findByBnoGreaterThan(Long bno, Pageable paging);
 
 }
--- src/test/java/org/dasd/persistence/BoardRepositoryTest.java
+++ src/test/java/org/dasd/persistence/BoardRepositoryTest.java
@@ -5,9 +5,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.Optional;
+import java.util.Collection;
+import java.util.List;
 import java.util.stream.Stream;
 
 import static org.junit.Assert.*;
@@ -80,6 +85,84 @@ public class BoardRepositoryTest {
 
         Board board=repo.findByBno(1L);
         repo.delete(board);
+
+    }
+
+    @Test
+    public void insert200(){
+
+        for (int i = 1; i <=200 ; i++) {
+            Board board=new Board();
+            board.setTitle("title : "+i);
+            board.setContent("content..."+i+" inserted");
+            board.setWriter("user0"+(i%10));
+            repo.save(board);
+        }
+
+
+    }
+
+    @Test
+    public void testByTitle(){
+        repo.findBoardByTitle("title : 177").forEach(board-> System.out.println(board));
+
+
+
+    }
+
+
+    @Test
+
+    public void testByWriter(){
+        Collection<Board> results=repo.findByWriter("user00");
+        results.forEach(
+                board-> System.out.println(board)
+        );
+    }
+
+    @Test
+    public void testByWriterContaining(){
+        Collection<Board>results=repo.findByWriterContaining("05");
+
+        results.forEach(board-> System.out.println(board));
+    }
+
+    @Test
+    public void testByTitleAndBno(){
+        Collection<Board>results=repo.findByTitleContainingAndBnoGreaterThan("5",50l);
+
+        results.forEach(board-> System.out.println(board));
+    }
+
+    @Test
+    public void testBnoOrderBy(){
+        Collection<Board>results=repo.findByBnoGreaterThanOrderByBnoDesc(90L);
+
+        results.forEach(board-> System.out.println(board));
+    }
+
+    @Test
+    public void testBnoOrderByPaging(){
+        Pageable paging= PageRequest.of(0,10);//<-첫 번째 페이지에 대해 10건의 데이터 가져오기
+        Collection<Board>results=repo.findByBnoGreaterThanOrderByBnoDesc(0L,paging);
+        results.forEach(board-> System.out.println(board));
+    }
+
+    @Test
+    public void testBnoPagingSort(){
+        Pageable paging=PageRequest.of(0,10, Sort.Direction.ASC,"bno");//<-첫 번쨰 페이지에 대해 10건의 데이터를 가져오되 bno 오름차순으로 가져온다.
+
+        Page<Board> results=repo.findByBnoGreaterThan(0L,paging);
+
+        System.out.println("Page Size" +results.getSize());
+        System.out.println("total pages: "+ results.getTotalPages());
+        System.out.println("Total count: "+results.getTotalElements());
+        System.out.println("next: "+ results.nextPageable());
+
+        List<Board>list= results.getContent();
+
+        list.forEach(board-> System.out.println(board));
+
 
     }
 }
