c110be667b229ac06daa6cfea75b7e56ea51e3ed
dasd412 <45190017+dasd412@users.noreply.github.com>
2020-12-20 11:28:37 +0900

    thymeleaf exercise 00

--- src/main/java/org/dasd/persistence/FreeBoardReplyRepository.java
+++
@@ -1,7 +0,0 @@
-package org.dasd.persistence;
-
-import org.dasd.domain.FreeBoardReply;
-import org.springframework.data.repository.CrudRepository;
-
-public interface FreeBoardReplyRepository extends CrudRepository<FreeBoardReply,Long> {
-}
--- src/main/java/org/dasd/persistence/ProfileRepository.java
+++
@@ -1,8 +0,0 @@
-package org.dasd.persistence;
-
-import org.dasd.domain.Profile;
-import org.springframework.data.repository.CrudRepository;
-
-//Member 객체를 통해서 Profile 객체를 처리할 수 없으므로 따로 만들었다.
-public interface ProfileRepository extends CrudRepository<Profile, Long> {
-}
