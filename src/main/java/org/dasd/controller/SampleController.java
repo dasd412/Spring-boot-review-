73d48925db46a03f9a68d39b338fef697356d55f
dasd412 <45190017+dasd412@users.noreply.github.com>
2020-12-20 19:38:58 +0900

    thymeleaf exercise 02

--- src/main/java/org/dasd/controller/SampleController.java
+++ src/main/java/org/dasd/controller/SampleController.java
@@ -37,4 +37,44 @@ public class SampleController {
         model.addAttribute("list",list);
 
     }
+
+    @GetMapping("/sample4")
+    public void sample4(Model model){
+
+        List<MemberVO>list=new ArrayList<>();
+
+        for (int i = 0; i <10 ; i++) {
+
+            list.add(new MemberVO(i,"u000"+i%3,"p000"+i%3,"wh"+i,new Timestamp(System.currentTimeMillis())));
+        }
+
+        model.addAttribute("list",list);
+    }
+
+    @GetMapping("/sample5")
+    public void sample5(Model model){
+
+        String result="success";
+
+        model.addAttribute("result",result);
+
+    }
+
+    @GetMapping("/sample6")
+    public void sample6(Model model){
+        List<MemberVO>list=new ArrayList<>();
+
+        for (int i = 0; i <10 ; i++) {
+            list.add(new MemberVO(i,"u0"+i,"p0"+i,"name"+i,new Timestamp(System.currentTimeMillis())));
+
+
+        }
+
+        model.addAttribute("list",list);
+
+        String result="success";
+
+        model.addAttribute("result",result);
+
+    }
 }
