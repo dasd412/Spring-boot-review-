16cf585e23e2b0a40f844a4372810ed5f700c25c
dasd412 <45190017+dasd412@users.noreply.github.com>
2020-12-20 19:08:23 +0900

    thymeleaf exercise 01

--- src/main/java/org/dasd/controller/SampleController.java
+++ src/main/java/org/dasd/controller/SampleController.java
@@ -1,15 +1,40 @@
 package org.dasd.controller;
 
+import org.dasd.domain.MemberVO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.sql.Timestamp;
+import java.util.*;
 
 @Controller
 public class SampleController {
 
     @GetMapping("/sample1")
     public void sample1(Model model){
-        model.addAttribute("greeting","Hello WORLD");
+        //model.addAttribute("greeting","Hello WORLD");
+
+        model.addAttribute("greeting","WHAT THE FUCK IS IT");
+    }
+
+    @GetMapping("/sample2")
+    public void sample2(Model model){
+        MemberVO vo=new MemberVO(123,"u00","p00","wh",new Timestamp(System.currentTimeMillis()));
+
+        model.addAttribute("vo",vo);
+    }
+
+    @GetMapping("/sample3")
+    public void sample3(Model model){
+
+        List<MemberVO>list=new ArrayList<>();
+
+        for (int i = 0; i <10 ; i++) {
+            list.add(new MemberVO(123,"u0"+i,"p0"+i,"wh"+i,new Timestamp(System.currentTimeMillis())));
+        }
+
+        model.addAttribute("list",list);
 
     }
 }
---
+++ src/main/java/org/dasd/domain/MemberVO.java
@@ -0,0 +1,20 @@
+package org.dasd.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.sql.Timestamp;
+
+
+@Data
+@AllArgsConstructor
+public class MemberVO {
+
+    private int mno;
+
+    private String mid;
+    private String mpw;
+    private String mname;
+    private Timestamp regdate;
+
+}
