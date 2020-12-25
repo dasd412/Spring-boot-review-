485d3f01d0949e9e6853393eb0a13fe3fc0aba7f
dasd412 <45190017+dasd412@users.noreply.github.com>
2020-12-25 14:50:19 +0900

    webMVC practice 03

--- src/main/java/org/dasd/controller/WebBoardController.java
+++ src/main/java/org/dasd/controller/WebBoardController.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -23,10 +24,10 @@ public class WebBoardController {
 
 
     @GetMapping("/list")
-    public void list(PageVO vo , Model model){
+    public void list(@ModelAttribute("pageVO") PageVO vo , Model model){
 
         Pageable page=vo.makePageable(0,"bno");
-        Page<WebBoard>result=repository.findAll(repository.makePredicate(null,null),page);
+        Page<WebBoard>result=repository.findAll(repository.makePredicate(vo.getType(),vo.getKeyword()),page);
 
         log.info(""+page);
         log.info(""+result);
